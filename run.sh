ad774519000eade638966293787d10dc1850371e
Nikola <mykolagalanix@gmail.com>
2023-06-14 13:58:55 +0300

    optimize run.sh for first deploy

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker compose up -d
+docker compose exec php composer update
 docker compose exec php composer install
+docker compose exec php bin/console lexik:jwt:generate-keypair
